e115b79ffbaf634d9f979deae83bf6027a2aa1c5
Yuri Kim <yurikim123@gmail.com>
2020-02-28 02:36:31 -1000

    check for Visa and MasterCard

--- detectNetwork.js
+++ detectNetwork.js
@@ -11,15 +11,24 @@ var detectNetwork = function(cardNumber) {
   // Note: `cardNumber` will always be a string
   var length = cardNumber.length;
   //prefix = first 2 digits
-  var prefix = cardNumber.slice(0,2);
+  var prefix = function(prefixLength) {
+  	return cardNumber.slice(0, prefixLength);
+  };
 
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
-  if (length === 14 && (prefix === '38' || prefix === '39')) return 'Diner\'s Club';
+  if (length === 14 && (prefix(2) === '38' || prefix(2) === '39')) return 'Diner\'s Club';
 
   // The American Express network always starts with a 34 or 37 and is 15 digits long
-  if (length === 15 && (prefix === '34' || prefix === '37')) return 'American Express';
+  if (length === 15 && (prefix(2) === '34' || prefix(2) === '37')) return 'American Express';
 
   // Once you've read this, go ahead and try to implement this function, then return to the console.
+
+  //Visa always has a prefix of 4 and a length of 13, 16, or 19.
+  if ((length === 13 || length === 16 || length === 19) && prefix(1) === '4') return 'Visa';
+
+  //MasterCard always has a prefix of 51, 52, 53, 54, or 55 and a length of 16.
+  if (length === 16 && (prefix(2) === '51' || prefix(2) === '52' || prefix(2) === '53' || prefix(2) === '54' || prefix(2) === '55')) 
+  		return 'MasterCard';
 };
 
 
