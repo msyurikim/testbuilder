dcb96178c7427e7ed54d7682c246b5d27bae47a2
Yuri Kim <yurikim123@gmail.com>
2020-02-28 02:59:13 -1000

    fixed cases for Visa

--- detectNetwork.test.js
+++ detectNetwork.test.js
@@ -90,15 +90,15 @@ describe('Visa', function() {
  
 
   it('has a prefix of 4 and a length of 13', function() {
-    assert(detectNetwork('4123456789012') !== 'Visa');
+    assert(detectNetwork('4123456789012') === 'Visa');
   });
 
   it('has a prefix of 4 and a length of 16', function() {
-    assert(detectNetwork('4123456789012345') !== 'Visa');
+    assert(detectNetwork('4123456789012345') === 'Visa');
   });
 
   it('has a prefix of 4 and a length of 19', function() {
-    assert(detectNetwork('4123456789012345678') !== 'Visa');
+    assert(detectNetwork('4123456789012345678') === 'Visa');
   });
 });
 
